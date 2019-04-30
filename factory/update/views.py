2092c8eeb5f6fc43e5753fdaa62479b61bf97e17
RockJohnson503 <836867547@qq.com>
2019-04-30 15:46:03 +0800

    添加update应用的视图

--- factory/update/views.py
+++ factory/update/views.py
@@ -1,3 +1,19 @@
+import os
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def update(request):
+    is_success = -1 # 返回结果,-1表示没有安装git,0表示更新失败.
+
+    file = os.path.dirname(os.path.abspath(""))
+    os.chdir(file)
+    if os.system("git help") == 0:
+        if os.system("git pull") == 0:
+            is_success = 1
+        else:
+            is_success = 0
+
+    return HttpResponse(is_success, content_type='application/json')
