7b583cc163d62af96586e043fc46454b96d20459
RockJohnson503 <836867547@qq.com>
2019-05-01 19:43:53 +0800

    添加turnoverZNQ的路由

--- factory/factory/urls.py
+++ factory/factory/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('update.urls')),
+    path('', include('turnoverZNQ.urls')),
+    path('update/', include('update.urls')),
     path('admin/', admin.site.urls),
 ]
