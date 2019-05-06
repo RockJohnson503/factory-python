af0040f2bd5e728fbc481e11464c4697b438c539
RockJohnson503 <836867547@qq.com>
2019-05-06 17:12:36 +0800

    产品明细添加的实现

--- factory/turnoverZNQ/views.py
+++ factory/turnoverZNQ/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseNotFound, HttpResponse
 from django.core.paginator import Paginator
+from django.db import transaction
 from django.urls import reverse
 from django.utils.timezone import now
 import json
@@ -53,14 +54,14 @@ def base_page(model, page):
 
 def product(request, page=1):
     # 产品的视图
-    products = User.objects.get(id=1).product_set.filter(is_delete=0).order_by('product_name')
+    products = User.objects.get(user_name='Rock').product_set.filter(is_delete=0).order_by('product_name')
     res = base_page(products, page)
     return render(request, 'turnoverZNQ/index.html', res) if res else HttpResponseNotFound()
 
 def detail(request, product_id, page=1):
     # 某产品的流水明细
-    products = get_object_or_404(User.objects.get(id=1).product_set, pk=product_id)
-    details = products.detail_set.order_by('time')
+    products = get_object_or_404(User.objects.get(user_name='Rock').product_set, pk=product_id)
+    details = products.detail_set.order_by('-time')
     res = base_page(details, page)
     if res:
         res['product_id'] = product_id
@@ -70,15 +71,16 @@ def product_add(request):
     # 添加产品
     args = json.loads(request.GET.get('args'))
     try:
-        User.objects.get(id=1).product_set.get(factory_name=args['factory'], product_type=args['id'], product_name=args['name'])
+        User.objects.get(user_name='Rock').product_set.get(factory_name=args['factory'], product_type=args['id'], product_name=args['name'])
         res = 'duplicate'
     except:
         # 是没有重复的产品
         try:
-            product = User.objects.get(id=1).product_set.create(factory_name=args['factory'], product_type=args['id'], product_name=args['name'],
+            with transaction.atomic():
+                product = User.objects.get(user_name='Rock').product_set.create(factory_name=args['factory'], product_type=args['id'], product_name=args['name'],
                                                       product_default=args['first'], product_now=args['now'], product_in=args['in'],
                                                       product_out=args['out'])
-            User.objects.get(id=1).log_set.create(log_time=now(), product_id=product.id, operate='添加产品')
+                User.objects.get(user_name='Rock').log_set.create(log_time=now(), product_id=product.id, operate='添加产品')
         except:
             res = 'err'
         res = 'ok'
@@ -89,8 +91,9 @@ def product_del(request):
     product_id = request.GET.get('id')
     res = 'ok'
     try:
-        User.objects.get(id=1).product_set.filter(id=product_id).update(is_delete=1)
-        User.objects.get(id=1).log_set.create(log_time=now(), product_id=product_id, operate='删除产品')
+        with transaction.atomic():
+            User.objects.get(user_name='Rock').product_set.filter(id=product_id).update(is_delete=1)
+            User.objects.get(user_name='Rock').log_set.create(log_time=now(), product_id=product_id, operate='删除产品')
     except:
         res = 'err'
     return HttpResponse(res)
@@ -99,18 +102,47 @@ def detail_add(request, product_id):
     # 添加流水账明细
     args = json.loads(request.GET.get('args'))
     try:
-        product = User.objects.get(id=1).product_set.get(id=product_id)
-        detail = product.detail_set.create(bill_id=args['bill_id'], operate=args['operate'], operate_num=args['num'], time=args['time'])
+        with transaction.atomic():
+            product = User.objects.get(user_name='Rock').product_set.get(id=product_id)
 
-        if args['operate'] == "入库":
-            product.product_in += args['num']
-            detail.productlave_set.create(amount=args['num'])
-        else:
-            product.product_out += args['num']
-        product.product_now = product.product_default + product.product_in - product.product_out
-        product.save()
+            if args['operate'] == "入库":
+                product.product_in += args['num']
+                detail = product.detail_set.create(bill_id=args['bill_id'], operate=args['operate'],
+                                                   operate_num=args['num'], time=args['time'])
+                detail.productlave_set.create(amount=args['num'])
+            else:
+                # 领料操作
+                if args['num'] > product.product_now:
+                    return HttpResponse('big')
 
-        User.objects.get(id=1).log_set.create(log_time=now(), detail_id=detail.id, operate=args['operate'])
+                product.product_out += args['num']
+                products = list(product.detail_set.filter(operate='入库').order_by('-time'))
+                if len(products) > 0:
+                    for i in products[:]:
+                        first_product = products.pop().productlave_set.all()[0]
+                        if first_product.amount == 0:
+                            continue
+                        if first_product.amount < args['num']:
+                            args['num'] -= first_product.amount # 操作产品数量
+                            detail = product.detail_set.create(bill_id=first_product.detail.bill_id, operate=args['operate'],
+                                                               operate_num=first_product.amount, time=args['time'])
+                            first_product.amount = 0
+                            first_product.save()
+                            continue
+
+                        detail = product.detail_set.create(bill_id=first_product.detail.bill_id, operate=args['operate'],
+                                                           operate_num=args['num'], time=args['time'])
+                        first_product.amount -= args['num']
+                        first_product.save()
+                        args['num'] = 0
+                        break
+                else:
+                    detail = product.detail_set.create(operate=args['operate'], operate_num=args['num'], time=args['time'])
+
+            product.product_now = product.product_default + product.product_in - product.product_out
+            product.save()
+
+            User.objects.get(user_name='Rock').log_set.create(log_time=now(), detail_id=detail.id, operate=args['operate'])
         res = 'ok'
     except:
         res = 'err'
