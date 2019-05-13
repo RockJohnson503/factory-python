7c20ac008ffea4b633f255b60593652eb757d9db
RockJohnson503 <836867547@qq.com>
2019-05-13 14:39:05 +0800

    新增更新后自动收集静态文件

--- factory/update/views.py
+++ factory/update/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def update(request):
-    is_success = -1 # 返回结果,-1表示没有安装git,0表示更新失败.
+    is_success = -1 # 返回结果,-1表示没有安装git,0表示更新失败,-2表示收集静态资源失败.
 
     file = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     os.chdir(file)
@@ -12,5 +12,8 @@ def update(request):
             is_success = 1
         else:
             is_success = 0
+
+    if os.system("python manage.py collectstatic --noinput") != 0:
+        is_success = -2
 
     return HttpResponse(is_success, content_type='application/json')
